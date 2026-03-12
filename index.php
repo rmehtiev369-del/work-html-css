0aa0c0b51d96064023509cd609ffe597920e45aa
you@example.com <you@example.com>
2026-03-12 13:30:48 +0300

    neew

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=for, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Register.php" method="post">
+     <input type="text" name="login" placeholder="login">
+     <input type="text" name="password" placeholder="password">
+     <input type="text" name="repet password" placeholder="repeat password">
+     <input type="text" name="email" placeholder="email">
+     <button type="submit">Зарегистрироватся</button>
+    </form>
+
+     <form action="login.php" method="post">
+     <input type="text" name="login" placeholder="login">
+     <input type="text" name="pass" placeholder="password">
+    <button type="submit">Войти</button>
+</body>
+</html>
